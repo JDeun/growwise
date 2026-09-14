21b977e49ed4f9dac88201af5b8a3150ff3666f1
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-14 16:58:10 +0900

    feat: export provider factory

--- src/growwise/model/__init__.py
+++ src/growwise/model/__init__.py
@@ -1,4 +1,5 @@
+from .factory import create_model_provider
 from .ollama import OllamaProvider
 from .provider import ModelProvider
 
-__all__ = ["ModelProvider", "OllamaProvider"]
+__all__ = ["ModelProvider", "OllamaProvider", "create_model_provider"]
