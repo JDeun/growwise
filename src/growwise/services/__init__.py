d5cb9e26be708f7425b4310184a4b4f8cc400e56
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-14 17:00:55 +0900

    feat: export infant activity service

--- src/growwise/services/__init__.py
+++ src/growwise/services/__init__.py
@@ -1,7 +1,11 @@
+from .infant import ActivitySuggestion, InfantActivityService, InfantActivitySuggestions
 from .observation import ObservationEnrichment, ObservationEnricher
 from .search import NaturalLanguageSearch, SearchPlan
 
 __all__ = [
+    "ActivitySuggestion",
+    "InfantActivityService",
+    "InfantActivitySuggestions",
     "NaturalLanguageSearch",
     "ObservationEnricher",
     "ObservationEnrichment",
