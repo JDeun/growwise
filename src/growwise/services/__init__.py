637e033a6222099969fe5e5a983405fdf09236e1
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-14 17:24:32 +0900

    feat: export conversation services

--- src/growwise/services/__init__.py
+++ src/growwise/services/__init__.py
@@ -1,4 +1,5 @@
 from .context import ChildContextService, ContextAnswer
+from .conversation import ConversationService, ConversationSession, ConversationTurn
 from .infant import ActivitySuggestion, InfantActivityService, InfantActivitySuggestions
 from .observation import ObservationEnricher, ObservationEnrichment
 from .search import NaturalLanguageSearch, SearchPlan
@@ -7,6 +8,9 @@ __all__ = [
     "ActivitySuggestion",
     "ChildContextService",
     "ContextAnswer",
+    "ConversationService",
+    "ConversationSession",
+    "ConversationTurn",
     "InfantActivityService",
     "InfantActivitySuggestions",
     "NaturalLanguageSearch",
