f1f068875aa7f1bc07900bb22c21c6af855a906e
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-15 07:31:52 +0900

    feat: export revision service

--- src/growwise/generators/__init__.py
+++ src/growwise/generators/__init__.py
@@ -1,9 +1,12 @@
 from .material import MaterialDraft, MaterialGenerationService
+from .revision import MaterialRevisionError, MaterialRevisionService
 from .scaffold import ScaffoldCheck, ScaffoldGuard
 
 __all__ = [
     "MaterialDraft",
     "MaterialGenerationService",
+    "MaterialRevisionError",
+    "MaterialRevisionService",
     "ScaffoldCheck",
     "ScaffoldGuard",
 ]
