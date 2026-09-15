7cba11a46c403297cc1e73b3ea63903ca253491f
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-15 20:32:43 +0900

    feat(curriculum): export curriculum adapter contract

--- src/growwise/adapters/__init__.py
+++ src/growwise/adapters/__init__.py
@@ -1,5 +1,6 @@
 from .base import AdapterResult, ExternalAdapterError, ExternalUnavailable
 from .cache import CachedPayload, SQLiteExternalCache
+from .curriculum import CurriculumRecord, PublicCurriculumAdapter
 from .data4library import Data4LibraryAdapter
 from .http import JsonHttpClient
 from .license_filter import filter_licensed, is_commercial_safe, normalize_license
@@ -8,11 +9,13 @@ from .overpass import OverpassAdapter
 __all__ = [
     "AdapterResult",
     "CachedPayload",
+    "CurriculumRecord",
     "Data4LibraryAdapter",
     "ExternalAdapterError",
     "ExternalUnavailable",
     "JsonHttpClient",
     "OverpassAdapter",
+    "PublicCurriculumAdapter",
     "SQLiteExternalCache",
     "filter_licensed",
     "is_commercial_safe",
