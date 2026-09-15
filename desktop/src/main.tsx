c2ef1c85d03ee039530ed7212ea29fc614b15087
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-15 15:28:16 +0900

    refactor(desktop): use hardened locale bootstrap

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -3,15 +3,14 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import PrintApprovedMaterials from "./PrintApprovedMaterials";
-import { applyDocumentLocale, loadLocale } from "./i18n";
+import { applyDocumentLocale, detectBrowserLocale } from "./i18n";
 import "./styles.css";
 import "./tokens.css";
 import "./brand.css";
 import "./activity-provenance.css";
 import "./print.css";
 
-const locale = loadLocale(window.localStorage, navigator.languages);
-applyDocumentLocale(locale);
+applyDocumentLocale(detectBrowserLocale());
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
