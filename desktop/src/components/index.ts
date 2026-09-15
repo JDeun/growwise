fb759e9d02d3d912d73bff72d288b96b0c4770f4
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-16 07:32:41 +0900

    fix(desktop): repair workspace barrel exports

--- desktop/src/components/index.ts
+++ desktop/src/components/index.ts
@@ -1,14 +1,21 @@
-export * from "./MaterialWorkspaceIntegration";
-export * from "./WorkspaceFrame";
-export * from "./WorkspaceNav";
-export * from "./WorkspacePlaceholder";
-export * from "./WorkspaceSection";
-export * from "./WorkspaceShell";
-export * from "./WorkspaceView";
-export * from "./workspaceDescriptions";
-export * from "./workspaceLabels";
-export * from "./workspaceLayout";
+export { CapabilityBanner } from "./CapabilityBanner";
+export { ConfirmDialog } from "./ConfirmDialog";
+export { EmptyState } from "./EmptyState";
+export { MaterialWorkspace } from "./MaterialWorkspace";
+export {
+  MaterialWorkspaceIntegration,
+  type MaterialWorkspaceController,
+} from "./MaterialWorkspace.integration";
+export { WorkspaceFrame } from "./WorkspaceFrame";
+export { WorkspaceNav } from "./WorkspaceNav";
+export { WorkspacePlaceholder } from "./WorkspacePlaceholder";
+export { WorkspaceSection } from "./WorkspaceSection";
+export { WorkspaceShell } from "./WorkspaceShell";
+export { WorkspaceView, type WorkspaceViewSlots } from "./WorkspaceView";
+export { WORKSPACE_DESCRIPTIONS } from "./workspaceDescriptions";
+export { WORKSPACE_LABELS } from "./workspaceLabels";
+export { WORKSPACE_LAYOUT } from "./workspaceLayout";
 export * from "./workspaceMigration";
 export * from "./workspaceStorage";
-export * from "./workspaceTypes";
-export * from "./workspaceViews";
+export type { WorkspaceView as WorkspaceViewName } from "./workspaceTypes";
+export { WORKSPACE_VIEWS } from "./workspaceViews";
