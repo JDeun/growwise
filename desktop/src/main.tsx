d82fd6ec922c030b0be52102d71180f97dfac33a
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-19 08:33:36 +0900

    fix: drop stale print toolbar import

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 import { ActiveChildProvider } from "./active-child-context";
-import PrintApprovedMaterials from "./PrintApprovedMaterials";
 import { WorkspaceShell } from "./components";
 import {
   DiscoveryWorkspace,
