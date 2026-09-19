d5e5519970af964c13aef102b1cf047e75e78a7b
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-19 11:35:49 +0900

    feat: export Korean public education adapters

--- src/growwise/adapters/__init__.py
+++ src/growwise/adapters/__init__.py
@@ -9,10 +9,16 @@ from .curriculum_resources import (
 from .data4library import Data4LibraryAdapter
 from .gbif import GbifSpeciesAdapter
 from .google_books import GoogleBooksAdapter
+from .heritage_palace import HeritagePalaceAdapter
 from .http import JsonHttpClient
+from .kma_weather import KmaWeatherAdapter, kma_grid_for, kma_observation_base
+from .krdict import KrDictAdapter
+from .museum import MuseumArtGalleryAdapter
+from .national_library import NationalLibraryIsbnAdapter
 from .license_filter import filter_licensed, is_commercial_safe, normalize_license
 from .nasa_images import NasaImagesAdapter
 from .official_curriculum_catalog import OfficialKoreanCurriculumCatalogAdapter
+from .curated_catalog import CuratedEducationCatalogAdapter
 from .openlibrary import OpenLibraryAdapter
 from .overpass import OverpassAdapter
 from .wikidata import WikidataAdapter
@@ -38,6 +44,14 @@ __all__ = [
     "WikidataAdapter",
     "WikimediaCommonsAdapter",
     "WikipediaAdapter",
+    "CuratedEducationCatalogAdapter",
+    "HeritagePalaceAdapter",
+    "KmaWeatherAdapter",
+    "KrDictAdapter",
+    "MuseumArtGalleryAdapter",
+    "NationalLibraryIsbnAdapter",
+    "kma_grid_for",
+    "kma_observation_base",
     "curriculum_records_to_resources",
     "curriculum_refs",
     "curriculum_resource_ref",
