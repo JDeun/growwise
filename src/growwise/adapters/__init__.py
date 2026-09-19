5b76763f0d0973ab2d279bf0d6045949c4628060
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-19 11:27:27 +0900

    feat: export education source adapters

--- src/growwise/adapters/__init__.py
+++ src/growwise/adapters/__init__.py
@@ -7,10 +7,17 @@ from .curriculum_resources import (
     curriculum_resource_ref,
 )
 from .data4library import Data4LibraryAdapter
+from .gbif import GbifSpeciesAdapter
+from .google_books import GoogleBooksAdapter
 from .http import JsonHttpClient
 from .license_filter import filter_licensed, is_commercial_safe, normalize_license
+from .nasa_images import NasaImagesAdapter
 from .official_curriculum_catalog import OfficialKoreanCurriculumCatalogAdapter
+from .openlibrary import OpenLibraryAdapter
 from .overpass import OverpassAdapter
+from .wikidata import WikidataAdapter
+from .wikimedia_commons import WikimediaCommonsAdapter
+from .wikipedia import WikipediaAdapter
 
 __all__ = [
     "AdapterResult",
@@ -19,11 +26,18 @@ __all__ = [
     "Data4LibraryAdapter",
     "ExternalAdapterError",
     "ExternalUnavailable",
+    "GbifSpeciesAdapter",
+    "GoogleBooksAdapter",
     "JsonHttpClient",
+    "NasaImagesAdapter",
     "OfficialKoreanCurriculumCatalogAdapter",
+    "OpenLibraryAdapter",
     "OverpassAdapter",
     "PublicCurriculumAdapter",
     "SQLiteExternalCache",
+    "WikidataAdapter",
+    "WikimediaCommonsAdapter",
+    "WikipediaAdapter",
     "curriculum_records_to_resources",
     "curriculum_refs",
     "curriculum_resource_ref",
