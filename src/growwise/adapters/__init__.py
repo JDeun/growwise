6b4a874bdbb918abfc9ca80807d50d73a1db3377
Kevin. Cho <165640725+JDeun@users.noreply.github.com>
2026-09-19 10:44:25 +0900

    feat: export education source adapters

--- src/growwise/adapters/__init__.py
+++ src/growwise/adapters/__init__.py
@@ -1,4 +1,10 @@
 from .base import AdapterResult, ExternalAdapterError, ExternalUnavailable
+from .book_sources import (
+    GlobalDigitalLibraryAdapter,
+    GoogleBooksAdapter,
+    GutendexAdapter,
+    NationalLibraryIsbnAdapter,
+)
 from .cache import CachedPayload, SQLiteExternalCache
 from .curriculum import CurriculumRecord, PublicCurriculumAdapter
 from .curriculum_resources import (
@@ -8,26 +14,74 @@ from .curriculum_resources import (
 )
 from .data4library import Data4LibraryAdapter
 from .http import JsonHttpClient
+from .knowledge_sources import (
+    GbifSpeciesAdapter,
+    NasaMediaAdapter,
+    NominatimAdapter,
+    OpenTopoDataAdapter,
+    TatoebaAdapter,
+    WikidataAdapter,
+    WikimediaCommonsAdapter,
+    WikipediaAdapter,
+)
+from .korean_sources import (
+    ConfiguredPublicDataAdapter,
+    KmaForecastAdapter,
+    KoreanHeritageAdapter,
+    KrdictAdapter,
+    OpenDictAdapter,
+)
 from .license_filter import filter_licensed, is_commercial_safe, normalize_license
 from .official_curriculum_catalog import OfficialKoreanCurriculumCatalogAdapter
 from .overpass import OverpassAdapter
+from .search_base import CachedSearchAdapter
+from .source_catalog import (
+    EDUCATION_SOURCE_CATALOG,
+    SOURCE_BY_ID,
+    EducationSourceSpec,
+    SourceIntegrationMode,
+    source_spec,
+)
 
 __all__ = [
     "AdapterResult",
     "CachedPayload",
+    "CachedSearchAdapter",
+    "ConfiguredPublicDataAdapter",
     "CurriculumRecord",
     "Data4LibraryAdapter",
+    "EDUCATION_SOURCE_CATALOG",
+    "EducationSourceSpec",
     "ExternalAdapterError",
     "ExternalUnavailable",
+    "GbifSpeciesAdapter",
+    "GlobalDigitalLibraryAdapter",
+    "GoogleBooksAdapter",
+    "GutendexAdapter",
     "JsonHttpClient",
+    "KmaForecastAdapter",
+    "KoreanHeritageAdapter",
+    "KrdictAdapter",
+    "NasaMediaAdapter",
+    "NationalLibraryIsbnAdapter",
+    "NominatimAdapter",
     "OfficialKoreanCurriculumCatalogAdapter",
+    "OpenDictAdapter",
+    "OpenTopoDataAdapter",
     "OverpassAdapter",
     "PublicCurriculumAdapter",
+    "SOURCE_BY_ID",
     "SQLiteExternalCache",
+    "SourceIntegrationMode",
+    "TatoebaAdapter",
+    "WikidataAdapter",
+    "WikimediaCommonsAdapter",
+    "WikipediaAdapter",
     "curriculum_records_to_resources",
     "curriculum_refs",
     "curriculum_resource_ref",
     "filter_licensed",
     "is_commercial_safe",
     "normalize_license",
+    "source_spec",
 ]
